bc0ed73f8db3514f54d76fd51d54f4d9e956393f
wdiazcarballo <papong@tu.ac.th>
2024-08-04 13:04:20 +0700

    fix define typo

--- config.h
+++ config.h
@@ -2,14 +2,14 @@
 #define CONFIG_H
 
 // Ensure UCS_HAVE_PARSER_SET_VALUE_TABLE_PREFIX is defined for compatibility
-#define UCS_HAVE_PARSER_SET_VALUE_TABLE_PREFIX
+#define UCS_HAVE_PARSER_SET_VALUE_TABLE_PREFIX 1
 
 // Define Allreduce Algorithms
-#define UCC_TL_UCP_ALLREDUCE_ALG_KNOMIAL       0
-#define UCC_TL_UCP_ALLREDUCE_ALG_SRA_KNOMIAL   1
-#define UCC_TL_UCP_ALLREDUCE_ALG_DBT           2
+#define UCC_TL_UCP_ALLREDUCE_ALG_KNOMIAL    0
+#define UCC_TL_UCP_ALLREDUCE_ALG_SRA_KNOMIAL    1
+#define UCC_TL_UCP_ALLREDUCE_ALG_DBT    2
 #define UCC_TL_UCP_ALLREDUCE_ALG_SLIDING_WINDOW 3
-#define UCC_TL_UCP_ALLREDUCE_ALG_LAST          4
+#define UCC_TL_UCP_ALLREDUCE_ALG_LAST   4
 
 // Define other configuration parameters
 #define MAX_MSG_SIZE (10 * 1024 * 1024) // 10 MB

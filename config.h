7c39726e58d8d251959ec091d89d362a5d3e4485
wdiazcarballo <papong@tu.ac.th>
2024-08-04 13:26:33 +0700

    comment out

--- config.h
+++ config.h
@@ -2,11 +2,11 @@
 #define CONFIG_H
 
 // Define Allreduce Algorithms
-#define  UCC_TL_UCP_ALLREDUCE_ALG_KNOMIAL 0
+/* #define  UCC_TL_UCP_ALLREDUCE_ALG_KNOMIAL 0
 #define  UCC_TL_UCP_ALLREDUCE_ALG_SRA_KNOMIA 1
 #define  UCC_TL_UCP_ALLREDUCE_ALG_SLIDING_WINDOW 2
 #define  UCC_TL_UCP_ALLREDUCE_ALG_DBT 3
-#define  UCC_TL_UCP_ALLREDUCE_ALG_LAST 4
+#define  UCC_TL_UCP_ALLREDUCE_ALG_LAST 4 */
 
 // Define other configuration parameters
 #define MAX_MSG_SIZE (10 * 1024 * 1024) // 10 MB
